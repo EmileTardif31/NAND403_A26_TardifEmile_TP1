5e4de030d5e331d6cf991e83fe0f55d1c6bd3952
EmileTardif31 <emiletardif31@gmail.com>
2026-09-21 15:33:11 -0400

    fonctionnement partiel de l'affichage dynamique

--- tardif_emile_tp1.py
+++ tardif_emile_tp1.py
@@ -8,20 +8,9 @@ from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QTableWidget, 
 
 app = QApplication(sys.argv)
 
-window = QWidget() # crée une fenetre
-window.show() # affiche la fenetre
-
-tableau = QTableWidget() # crée un tableau simple
-tableau.setRowCount(10) # set le nombre de row
-tableau.setColumnCount(3) # set le nombre de colonnes
-# set des noms aux colonnes
-tableau.setHorizontalHeaderLabels([
-    "Nom",
-    "Age",
-    "Ville"
-])
-
-tableau.setItem(0,1,QTableWidgetItem("Alice")) # set l'élément du tableau a la ligne 0, colonne 1, on lui donne la valeur "Alice"
+# -- affiche une fenetre, mais on a juste besoin d'un tableau --
+# window = QWidget() # crée une fenetre
+# window.show() # affiche la fenetre
 
 # sys.exit(app.exec())  ---- il faut mettre ca la fin du programme, et le code apres s'exécute des que la window se ferme
 
@@ -37,10 +26,41 @@ large_fichier = open("data_large.json", "r") # on le stocke dans une variable qu
 with open("data_small.json", "r") as small_fichier:        # with permet de ouvrir et fermer le json sans avoir a mettre open au debut et close a la fin, et dans la fonction with on a acces au fichier ouvert
     donnees_small = json.load(small_fichier) # il faut load le fichier dans une variable pour accéder aux données
 
+with open("data_large.json", "r") as large_fichier:
+    donnees_large = json.load(large_fichier)
+
 # print(donnees_small) # retourne toutes les données
 # print(donnees_small[0]) # retourne les données dans le 1er index
 # print(donnees_small[0]["nom"]) # retourne le nom dans le 1er index des données
 
-for i in donnees_small: # on peut boucler dans les données pour accéder aux variables de chaque index
-    print(i["nom"]) # retourne tous les noms dans le fichier
+# for i in donnees_small: # on peut boucler dans les données pour accéder aux variables de chaque index
+#     print(i["nom"]) # retourne tous les noms dans le fichier
 
+# -- tableau dynamique --
+
+tableau = QTableWidget() # crée un tableau simple
+tableau_choisi = donnees_small # le tableau qu'on va afficher (on change la valeur selon le tableau quon veut)
+
+tableau.setRowCount(len(tableau_choisi)) # set le nombre de row en checkant le nombre de key dans le tableau
+tableau.setColumnCount(len(tableau_choisi[0])) # set le nombre de colonnes en checkant le nombre de variables dans la 1ere key (en assumant que toutes les key ont le meme nombre de variables)
+# set des noms aux colonnes
+tableau.setHorizontalHeaderLabels(tableau_choisi[0].keys())
+
+# for i in tableau_choisi:
+#     for k in i:
+#         #tableau.setItem(k,i,QTableWidgetItem(i[k]))
+#         print(k)
+#         print(i[k])
+#     # for k in range(len(i)):
+#     #     tableau.setItem(0,k,QTableWidgetItem("Alice"))
+#     #     print(i)
+
+for ligne, i in enumerate(tableau_choisi):
+    for colonne, k in enumerate(i):
+        tableau.setItem(ligne, colonne, QTableWidgetItem(i[k]))
+
+# tableau.setItem(0,0,QTableWidgetItem("Alice")) # set l'élément du tableau a la ligne 0, colonne 1, on lui donne la valeur "Alice"
+
+tableau.show()
+
+sys.exit(app.exec())
