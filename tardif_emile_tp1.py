bc729794e51468d77fdc0f5f3a0f4c2162dd3920
EmileTardif31 <emiletardif31@gmail.com>
2026-09-21 18:05:28 -0400

    commentaires et hopefully fin du tp

--- tardif_emile_tp1.py
+++ tardif_emile_tp1.py
@@ -4,20 +4,19 @@ import json # nécessaire pour utiliser les fichiers json
 
 # ------------ Json setup ----------------
 
-fichier = input("Fichier json : ")
+fichier = input("Fichier json : ") # on entre le fichier json qu'on veut inspecter
 
-if os.path.exists(fichier):
-    print("Fichier valide")
+if os.path.exists(fichier): # on vérifie si le fichier existe
+    print("Fichier valide.")
 else:
-    print("Erreur de nom de fichier")
-    exit()
+    print("Ce fichier est introuvable ou n'existe pas.")
+    exit() # on ferme le programme si le fichier 
 
 small_fichier = open(fichier, "r") # ouvre un fichier (param1) en lecture (param2)
-poids_small = os.path.getsize(fichier) / 1024
+poids_small = os.path.getsize(fichier) / 1024 # obtient la taille du fichier en octet, on divise par 1024 pour l'avoir en Ko
 # print(small_fichier.read()) # on affiche le contenu du fichier
 
 # mais pour accéder aux variables il faut load le fichier
-
 with open(fichier, "r", encoding="utf-8") as small_fichier:        # with permet de ouvrir et fermer le json sans avoir a mettre open au debut et close a la fin, et dans la fonction with on a acces au fichier ouvert
     donnees_small = json.load(small_fichier) # il faut load le fichier dans une variable pour accéder aux données
 
@@ -33,7 +32,7 @@ with open(fichier, "r", encoding="utf-8") as small_fichier:        # with permet
 # les lignes pour importer pyside6
 from PySide6.QtWidgets import QWidget, QApplication, QTableWidget, QTableWidgetItem, QLineEdit, QMainWindow, QVBoxLayout, QLabel
 
-app = QApplication(sys.argv)
+app = QApplication(sys.argv) # crée l'application graphique pyside6 qui va permettre d'afficher l'interface
 
 # -- affiche une fenetre, mais on a juste besoin d'un tableau --
 window = QMainWindow() # crée une fenetre
@@ -62,6 +61,7 @@ tableau.setColumnCount(len(tableau_choisi[0])) # set le nombre de colonnes en ch
 # set des noms aux colonnes
 tableau.setHorizontalHeaderLabels(tableau_choisi[0].keys())
 
+# boucle pour accéder à chaque cellule
 for ligne, i in enumerate(tableau_choisi):             # sort chaque élément du tableau avec l'élément stocké en i
     for colonne, k in enumerate(i):                    # sort chaque élément dans i stocké dans k
         tableau.setItem(ligne, colonne, QTableWidgetItem(str(i[k])))
@@ -74,40 +74,39 @@ tableau.setSortingEnabled(True) # debloque la fonction native de tri du tableau
 
 # -- recherche --
 
-recherche = QLineEdit()
+recherche = QLineEdit() # truc pour créer une barre de recherche
 recherche.setPlaceholderText("Rechercher...")
 
 # fonction pour afficher / retirer le texte
 def update_search(texte):
 
+    # boucle pour accéder au contenu de chaque cellule
     for ligne, i in enumerate(tableau_choisi):
-        trouve = False
+        trouve = False # on se mets un bool pour arrêter la boucle si au moins 1 élément dans la ligne est correspondant
         for element, k in enumerate(i):
             if(trouve != True):
-                if texte.lower() in str(i[k]).lower():
+                if texte.lower() in str(i[k]).lower(): # check si le texte correspond
                     tableau.setRowHidden(ligne, False)
                     trouve = True
-                elif texte == "":
+                elif texte == "": # check si la barre est vide
                     tableau.setRowHidden(ligne, False)
                     print("recherche vide")
-                else:
+                else: # si aucune correspondance on cache la ligne
                     tableau.setRowHidden(ligne, True)
 
 
-recherche.textChanged.connect(update_search)
+recherche.textChanged.connect(update_search) # fonction appelée quand le texte change dans la barre de recherche, on valide à chaque modification
 
 # -- infos du fichier --
 infos = QLabel()
-# infos.setText("les infos du fichier")
-
-infos.setText(
+infos.setText( # change le texte a afficher dans le label (on peut mettre plusieurs fstrings pour plus de texte)
     f"Nom : {fichier}\n"
-    f"Taille : {poids_small} Kb\n"
+    f"Taille : {poids_small} Ko\n"
     f"Nombre d'éléments : {len(tableau_choisi)}"
 )
 
 # -- ajouter les éléments visuels au layout dans l'ordre qu'on veut
-layout.addWidget(recherche)
+layout.addWidget(recherche) # layout.addWidget permet d'ajouter des éléments à l'interface
 layout.addWidget(infos)
 layout.addWidget(tableau)
 
