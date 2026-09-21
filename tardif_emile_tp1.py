fa7af1ad6732d302ab938a2cca889b00cad8b66f
EmileTardif31 <emiletardif31@gmail.com>
2026-09-21 16:25:36 -0400

    on peut entrer dans la console le fichier et l'affichage est dynamique

--- tardif_emile_tp1.py
+++ tardif_emile_tp1.py
@@ -1,33 +1,25 @@
 import sys
+import os
 import json # nécessaire pour utiliser les fichiers json
-
-# ------------- PySide6 setup -------------------------
-
-# les lignes pour importer pyside6
-from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QTableWidget, QTableWidgetItem
-
-app = QApplication(sys.argv)
-
-# -- affiche une fenetre, mais on a juste besoin d'un tableau --
-# window = QWidget() # crée une fenetre
-# window.show() # affiche la fenetre
-
-# sys.exit(app.exec())  ---- il faut mettre ca la fin du programme, et le code apres s'exécute des que la window se ferme
 
 # ------------ Json setup ----------------
 
-small_fichier = open("data_small.json", "r") # ouvre un fichier (param1) en lecture (param2)
-large_fichier = open("data_large.json", "r") # on le stocke dans une variable qui devient un objet fichier (on peut lire toute mais difficilement accéder aux données (voir methode plus bas))
+fichier = input("Fichier json : ")
 
+if os.path.exists(fichier):
+    print("Fichier valide")
+else:
+    print("Erreur de nom de fichier")
+    exit()
+
+small_fichier = open(fichier, "r") # ouvre un fichier (param1) en lecture (param2)
+poids_small = os.path.getsize(fichier) / 1024
 # print(small_fichier.read()) # on affiche le contenu du fichier
 
 # mais pour accéder aux variables il faut load le fichier
 
-with open("data_small.json", "r", encoding="utf-8") as small_fichier:        # with permet de ouvrir et fermer le json sans avoir a mettre open au debut et close a la fin, et dans la fonction with on a acces au fichier ouvert
+with open(fichier, "r", encoding="utf-8") as small_fichier:        # with permet de ouvrir et fermer le json sans avoir a mettre open au debut et close a la fin, et dans la fonction with on a acces au fichier ouvert
     donnees_small = json.load(small_fichier) # il faut load le fichier dans une variable pour accéder aux données
-
-with open("data_large.json", "r", encoding="utf-8") as large_fichier:
-    donnees_large = json.load(large_fichier)
 
 # print(donnees_small) # retourne toutes les données
 # print(donnees_small[0]) # retourne les données dans le 1er index
@@ -35,6 +27,29 @@ with open("data_large.json", "r", encoding="utf-8") as large_fichier:
 
 # for i in donnees_small: # on peut boucler dans les données pour accéder aux variables de chaque index
 #     print(i["nom"]) # retourne tous les noms dans le fichier
+
+# ------------- PySide6 setup -------------------------
+
+# les lignes pour importer pyside6
+from PySide6.QtWidgets import QWidget, QApplication, QTableWidget, QTableWidgetItem, QLineEdit, QMainWindow, QVBoxLayout, QLabel
+
+app = QApplication(sys.argv)
+
+# -- affiche une fenetre, mais on a juste besoin d'un tableau --
+window = QMainWindow() # crée une fenetre
+window.show() # affiche la fenetre
+
+# -- widget central qui sert de conteneur pour les autres choses
+central = QWidget()
+window.setCentralWidget(central)
+
+# -- layout pour les choses dans la fenetre --
+layout = QVBoxLayout()
+central.setLayout(layout)
+layout.setContentsMargins(10, 10, 10, 10)
+layout.setSpacing(20)
+
+# sys.exit(app.exec())  ---- il faut mettre ca la fin du programme, et le code apres s'exécute des que la window se ferme
 
 # -- tableau dynamique --
 
@@ -51,8 +66,31 @@ for ligne, i in enumerate(tableau_choisi):
     for colonne, k in enumerate(i):
         tableau.setItem(ligne, colonne, QTableWidgetItem(str(i[k])))
 
+tableau.setSortingEnabled(True)
+
 # tableau.setItem(0,0,QTableWidgetItem("Alice")) # set l'élément du tableau a la ligne 0, colonne 1, on lui donne la valeur "Alice"
 
-tableau.show()
+# tableau.show()
+
+# -- recherche --
+
+recherche = QLineEdit()
+recherche.setPlaceholderText("Rechercher...")
+# recherche.show()
+
+# -- infos du fichier --
+infos = QLabel()
+# infos.setText("les infos du fichier")
+
+infos.setText(
+    f"Nom : {fichier}\n"
+    f"Taille : {poids_small} Kb\n"
+    f"Nombre d'éléments : {len(tableau_choisi)}"
+)
+
+# -- ajouter les éléments visuels au layout dans l'ordre qu'on veut
+layout.addWidget(recherche)
+layout.addWidget(infos)
+layout.addWidget(tableau)
 
 sys.exit(app.exec()) # -- empêche la suite du code de s'exécuter tant qu'on a pas fermé la fenêtre
